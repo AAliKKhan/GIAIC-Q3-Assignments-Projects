0215f3d81acbf21a71b424cddbd05282e8d8c64f
AAliKKhan <muhammda014@gmail.com>
2025-03-23 04:10:02 +0500

    completed 03 feet to inches

---
+++ Project 4 assignments/assignments0to5/01_expressions/03_feet_to_inches.py
@@ -0,0 +1,13 @@
+def main():
+
+    feet: float = float(input("Enter length in feet: "))
+
+
+    inches: float = feet * 12
+
+
+    print(f"{feet} feet is equal to {inches} inches.")
+
+if __name__ == '__main__':
+    main()
+5
