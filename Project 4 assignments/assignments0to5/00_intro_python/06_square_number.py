4e24dda7f5fb32615c323ff7add1e10af9551cc9
AAliKKhan <muhammda014@gmail.com>
2025-03-23 03:54:17 +0500

    completed 06 square of number

---
+++ Project 4 assignments/assignments0to5/00_intro_python/06_square_number.py
@@ -0,0 +1,11 @@
+def main():
+    numb=float(input("Enter the number to calculate square :"))
+    square=numb*numb
+    print(str(numb) + " squared is " + str(square)) 
+     
+
+
+
+
+if __name__ == '__main__':
+    main()
