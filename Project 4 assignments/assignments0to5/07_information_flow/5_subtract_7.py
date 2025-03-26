21608da157efdbffe695723cfbb336faa28af68e
AAliKKhan <muhammda014@gmail.com>
2025-03-26 17:25:54 +0500

    completed 05 subtract 7

---
+++ Project 4 assignments/assignments0to5/07_information_flow/5_subtract_7.py
@@ -0,0 +1,10 @@
+def main():
+    num = 7
+    result = subtract_seven(num)
+    print("This should be zero:", result)
+
+def subtract_seven(num):
+    return num - 7 
+
+if __name__ == '__main__':
+    main()
