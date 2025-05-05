6fc0ca913d14e1ba6b938808464f63b44b85459e
AAliKKhan <muhammda014@gmail.com>
2025-05-05 10:57:16 +0500

    completed part 3

---
+++ 06_Build_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series/3.py
@@ -0,0 +1,19 @@
+# 3. Public Variables and Methods
+# Assignment:
+# Create a class Car with a public variable brand and a public method start(). Instantiate the class and access both from outside the cl
+
+class Car:
+
+  def __init__(self, brand):
+    self.brand = brand  
+
+
+  def start(self):
+    print(f"The {self.brand} car has been purchased.")  
+
+my_car = Car("Maserati")
+
+
+print("Car Brand:", my_car.brand)
+
+my_car.start()
